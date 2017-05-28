731c86d1c85f7af0bbf6f8d1879246c6d0afdf44
JiaoZhe <946505120@qq.com>
2017-05-29 07:11:26 +0800

    change banner

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -1,7 +1,7 @@
 require('../../less/common.less');
 require('../../less/index.less');
 require('swiper/dist/css/swiper.min.css')
-var Chart = require('../common/chart.js');	
+var echarts = require('echarts');
 	 var Swiper = require('swiper');
 	 require('../common/ScrollSpy');
 	 require('./common.js');
@@ -47,22 +47,23 @@ var mySwiper = new Swiper ('.swiper-container', {
 		    // 如果需要前进后退按钮
 		    nextButton: '.swiper-button-next',
 		    prevButton: '.swiper-button-prev',
- 		})
+ })
 		
 
 
 
 //发展历程
-
-	var timerLine = $('.time_item'),
-     timer = $('.timer_content');
+	var timerLine = $('.path_index'),
+     timer = $('.history_path_text');
 		var index = 0,lTimer = null;
 		timerLine.removeClass('on');
 		function slider(){
-			timerLine.removeClass('on');
+			timerLine.removeClass('on').next().removeClass('ons');
 			timer.removeClass('on');
-			timerLine.eq(index).addClass('on');
-			timer.eq(index).addClass('on');;
+			timer.hide();
+			timerLine.eq(index).addClass('on').next().addClass('ons');
+			// timer.eq(index).addClass('on');
+			timer.eq(index).show();
 			index++;
 			if(index>=5){
 				index = 0;
@@ -75,14 +76,85 @@ var mySwiper = new Swiper ('.swiper-container', {
 			var _this = $(this);
 			_this.click(function(){
 				clearTimeout(lTimer);
+				timer.hide();
 				timerLine.removeClass('on');
 				timer.removeClass('on');
-				_this.addClass('on');
-				timer.eq(i).addClass('on');
+				_this.addClass('on').next().addClass('ons');
+				timer.eq(i).show();;
 			})
 		})
 	
+function drawCanvasOne() {
+	var t = document.getElementById("wave"),
+		e = echarts.init(t),
+		i = e.getZr(),
+		a = e.getWidth(),
+		c = e.getHeight(),
+		o = new echarts.graphic.Group;
+	i.add(o);
+	var n = 3;
+	800 > a && (n = 2);
+	for (var r = 0; 3 > r; r++) {
+		for (var s = [], l = 0; n + 1 >= l; l++) {
+			var h = c / 10 * r + c / 6,
+				d = Math.random() * c / 8 + h,
+				g = c - Math.random() * c / 8 - h,
+				m = [
+					[2 * l * a / n / 2, r % 2 ? d : g],
+					[(2 * l + 1) * a / n / 2, r % 2 ? g : d]
+				];
+			s.push(m[0], m[1])
+		}
+		console.log(s.toString())
+		var u = new echarts.graphic.Polyline({
+			shape: {
+				points: r==1 ? s : s,
+				smooth: .4
+			},
+			style: {
+				stroke: "#fff",
+				opacity: 1 / (r + 1),
+				lineWidth: 1.2 / (r + 1) + .8
+			},
+			silent: !0,
+			position: [-r * a / 8, 35 * -(r - .5)]
+		}),
+			y = new echarts.graphic.Rect({
+				shape: {
+					x: 0,
+					y: 0,
+					width: 0,
+					height: c
+				},
+				position: [r * a / 8, 0]
+			});
+		o.add(u), y.animateTo({
+			shape: {
+				width: a
+			}
+		}, 2e3, 800 * Math.random()), u.setClipPath(y), n += 1
+	}
+	// $(window).on("resize", function() {
+	// 	var t = e.getWidth(),
+	// 		i = e.getHeight();
+	// 	e.resize();
+	// 	var a = e.getWidth(),
+	// 		c = e.getHeight();
+	// 	y.setShape({
+	// 		width: a,
+	// 		height: c
+	// 	});
+	// 	var n = a / t,
+	// 		r = c / i;
+	// 	o.eachChild(function(t) {
+	// 		t.position[0] *= n, t.position[1] *= r, t.shape.points.forEach(function(t) {
+	// 			t[0] *= n, t[1] *= r
+	// 		}), t.dirty(!0)
+	// 	})
+	// })
+}
 
+drawCanvasOne()
 
 
 });
@@ -91,34 +163,34 @@ var mySwiper = new Swiper ('.swiper-container', {
 
 
 // 图表
-var ctx = $("#myChart").get(0).getContext("2d");
-var data = {
-	labels : [0,48,40,19,96,27,100,40,69,56,59,90,81,40,19,0],
-	datasets : [
-		{
-			fillColor : "rgba(81,187,229,0.8)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			// pointStrokeColor : "#fff",
-			data : [0,59,90,81,56,55,40,69,56,59,90,81,40,19,0]
-		},
-		{
-			fillColor : "rgba(79,152,241,.8)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			// pointStrokeColor : "#fff",
-			data : [0,48,40,19,96,27,100,40,69,56,59,90,81,40,19,0]
-		}
-	]
-}
-console.log(Chart)
-var d = new Chart(ctx).Line(data,{scaleShowLabels : false,
- // scaleFontColor : "transparent",
- // scaleShowGridLines:false,
-	scaleLineWidth:0,
-	pointDotStrokeWidth:0,
-	datasetStrokeWidth:2,
-	// pointDotRadius:5,
-	datasetStrokeWidth : 1
-	}
-);
+// var ctx = $("#myChart").get(0).getContext("2d");
+// var data = {
+// 	labels : [0,48,40,19,96,27,100,40,69,56,59,90,81,40,19,0],
+// 	datasets : [
+// 		{
+// 			fillColor : "rgba(81,187,229,0.8)",
+// 			strokeColor : "rgba(220,220,220,1)",
+// 			pointColor : "rgba(220,220,220,1)",
+// 			// pointStrokeColor : "#fff",
+// 			data : [0,59,90,81,56,55,40,69,56,59,90,81,40,19,0]
+// 		},
+// 		{
+// 			fillColor : "rgba(79,152,241,.8)",
+// 			strokeColor : "rgba(151,187,205,1)",
+// 			pointColor : "rgba(151,187,205,1)",
+// 			// pointStrokeColor : "#fff",
+// 			data : [0,48,40,19,96,27,100,40,69,56,59,90,81,40,19,0]
+// 		}
+// 	]
+// }
+
+// var d = new Chart(ctx).Line(data,{scaleShowLabels : false,
+//  // scaleFontColor : "transparent",
+//  // scaleShowGridLines:false,
+// 	scaleLineWidth:0,
+// 	pointDotStrokeWidth:0,
+// 	datasetStrokeWidth:2,
+// 	// pointDotRadius:5,
+// 	datasetStrokeWidth : 1
+// 	}
+// );
